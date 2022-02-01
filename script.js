a0f9fdc8116c854db6661df5189a914cc3a0da43
Luiz Gustavo Santos Farias <lgsfarias.dev@gmail.com>
2022-02-01 17:18:19 -0300

    distribuição de cartas aos pares e função de embaralhar as cartas

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+let cartas = [ 'unicornparrot','unicornparrot','tripletsparrot','tripletsparrot','revertitparrot','revertitparrot','metalparrot','metalparrot', 'fiestaparrot', 'fiestaparrot', 'explodyparrot','explodyparrot', 'bobrossparrot', 'bobrossparrot']
 let verificar = false;
 while (!verificar){
     let numCartas = parseInt(prompt("quantas cartas?"));
     if (numCartas%2==0 && numCartas>=4 && numCartas<=14){
         verificar=true;
+        
+        let deck = cartas.slice(0,numCartas);
+        deck.sort(() => Math.random() - 0.5); // Após esta linha, a minhaArray estará embaralhada
+
         let conteudo=''
-        for (let i=0;i<numCartas;i++){
-            conteudo+='<div class="carta" onclick="viraCarta(this)"><div class="cartaFrente"><img src="./gif/unicornparrot.gif"></div><div class="cartaTras"><img src="./img/front.png"></div></div>'
+        for (let i=0;i<deck.length;i++){
+            conteudo+=`<div class="carta" onclick="viraCarta(this)"><div class="cartaFrente"><img src='./gif/${deck[i]}.gif'></div><div class="cartaTras"><img src="./img/front.png"></div></div>`
         }
         document.querySelector("main").innerHTML=conteudo
     }
