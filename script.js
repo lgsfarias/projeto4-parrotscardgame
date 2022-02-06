1dbb2e3c006eb3b316688e79e254183f182a87fb
Luiz Gustavo Santos Farias <lgsfarias.dev@gmail.com>
2022-02-06 00:52:57 -0300

    Version 2.0

--- script.js
+++ script.js
@@ -1,107 +1,114 @@
-const cartas = [ 'unicornparrot','unicornparrot','tripletsparrot','tripletsparrot','revertitparrot','revertitparrot','metalparrot','metalparrot', 'fiestaparrot', 'fiestaparrot', 'explodyparrot','explodyparrot', 'bobrossparrot', 'bobrossparrot']
+const cards = [ 'unicornparrot','unicornparrot','tripletsparrot','tripletsparrot','revertitparrot','revertitparrot','metalparrot','metalparrot', 'fiestaparrot', 'fiestaparrot', 'explodyparrot','explodyparrot', 'bobrossparrot', 'bobrossparrot']
 
-let primeiraCarta
-let segundaCarta
-let contador // número de jogadas
-let lock // caso True o usuário não poderá selecionar cartas
-let tempo 
-let relogio
+let firstCard
+let secondCard
+let counter // number of moves
+let lock // if True, the user will not be able to select cards
+let time 
+let clock
 let myTimer
 
-function iniciar () {
-    // Esta função é invocada assim que a pagina é carregada e quando o usuário decide jogar novamente
+function start () {
+    // this function is called as soon as the page is loaded and when the user decides to play again
 
-    let numCartas = parseInt(prompt("quantas cartas você deseja? (numeros pares de 4 a 14)"));// Pergunta-se ao usuário com quantas cartas ele quer jogar 
+    let numCards = parseInt(prompt("[PT-BR] Quantas cartas você deseja? (numeros pares de 4 a 14)\n\n[EN] How many cards do you want? (even numbers from 4 to 14)"));// The user is asked how many cards he wants to play with
     
-    while (numCartas%2!==0 || numCartas<4 || numCartas>14){
-        // Repete-se a pergunta até que o número de cartas seja válido
-        numCartas = parseInt(prompt("quantas cartas você deseja? (numeros pares de 4 a 14)"));
+    while (numCards%2!==0 || numCards<4 || numCards>14){
+        numCards = parseInt(prompt("[PT-BR] Quantas cartas você deseja? (numeros pares de 4 a 14)\n\n[EN] How many cards do you want? (even numbers from 4 to 14)"));
     }
     
-    let deck = cartas.slice(0,numCartas); // Cria-se a partir das cartas disponíveis um array com o número de cartas escolhido pelo usuário 
-    deck.sort(() => Math.random() - 0.5); // Após esta linha, a minhaArray estará embaralhada
+    let deck = cards.slice(0,numCards);
+    deck.sort(() => Math.random() - 0.5); // After this line, deck will be scrambled
 
-    let conteudo=''
+    let content=''
     for (let i=0;i<deck.length;i++){
-        conteudo+=`<div data-identifier="card" class="carta" onclick="viraCarta(this)"><div data-identifier=ront-face" class="cartaFrente"><img src='./gif/${deck[i]}.gif'></div><div data-identifier="back-face" class="cartaTras"><img src="./img/front.png"></div></div>`
+        content+=`<div data-identifier="card" class="card" onclick="flipCard(this)">
+            <div data-identifier=front-face" class="cardFront">
+                <img src='./gif/${deck[i]}.gif' alt="front face">
+            </div>
+            <div data-identifier="back-face" class="cardBack">
+                <img src="./img/front.png" alt="back face">
+            </div>
+        </div>`
     }
-    document.querySelector("main").innerHTML=conteudo; // Mostra as cartas ja embaralhadas no arquivo HTML
 
-    primeiraCarta=null;
-    segundaCarta=null;
-    contador = 0;
+    document.querySelector("main").innerHTML=content; 
+    
+    firstCard=null;
+    secondCard=null;
+    counter = 0;
     lock=false;
-    tempo=0;
+    time=0;
 
-    document.querySelector("header").innerHTML="<h1>PARROT CARD GAME</h1><p>0</p>" // Mostra o cabeçalho no HTML
+    document.querySelector("header").innerHTML="<h1>PARROT CARD GAME</h1><p>0</p>"
     
     myTimer = setInterval(() => {
-        // Cria um cronômetro na tela e atualiza a cada 1 segundo
-        tempo++
-        relogio = document.querySelector("header p");
-        relogio.innerHTML = tempo
+        // Creates an on-screen timer and updates every 1 second
+        time++
+        clock = document.querySelector("header p");
+        clock.innerHTML = time
     }, 1000);
 
 }
 
 
-function viraCarta (carta) {
-    if(!lock && !carta.classList.contains("flip")){ // Verifica se a carta ja está virada ou se está em lock (será explicado a frente)
-        carta.classList.add("flip") // Vira a carta
-        if (!primeiraCarta){ 
-            primeiraCarta = carta.querySelector(".cartaFrente img"); // Seleciona a primeira carta
-        }else if(!segundaCarta){
-            segundaCarta = carta.querySelector(".cartaFrente img"); // Seleciona a segunda carta
-            if (primeiraCarta.src == segundaCarta.src){ // verifica se as duas cartas tem a mesma imagem
-                // console.log("deu match");
-                primeiraCarta.parentNode.parentNode.classList.add("match"); // Guarda a informção que a carta ja tem um par
-                segundaCarta.parentNode.parentNode.classList.add("match"); // Guarda a informção que a carta ja tem um par
-                primeiraCarta=null; 
-                segundaCarta=null;
-                contador++; // adiciona 1 no contador de jogadas
-                if (verificaFim()){ // verifica se o jogo já terminou
-                    clearInterval(myTimer); // encerra o cronômetro na tela
-                    setTimeout( () => {
-                        alert(`acabou em ${contador*2} jogadas e em ${document.querySelector("header p").innerHTML} segundos!`); // emite um alerta com o numero de jogadas e o tempo que o usuário levou
-                        const novamenteOptions = ["s","n",null];
-                        let novamente=prompt("quer jogar novamente? (s ou n)").toLowerCase() ; // pergunta se o usuário quer jogar novamente
-                        while (!novamenteOptions.includes(novamente)){ // verifica se foi uma resposta válida
-                            novamente=prompt("quer jogar novamente? (s ou n)").toLowerCase();
-                        }
-                        if(novamente=="s"){ // caso responda com "s" o jogo reinicia
-                            iniciar();
-                        }
-                    },100);     
+function flipCard (card) {
+    if(!lock && !card.classList.contains("flip")){
+        card.classList.add("flip")
+        
+        if (!firstCard){ 
+            firstCard = card.querySelector(".cardFront img"); // Select the first card
+        }else if(!secondCard){
+            secondCard = card.querySelector(".cardFront img"); // Select the second card
+            if (firstCard.src == secondCard.src){ // checks if the two cards have the same image
+                firstCard.parentNode.parentNode.classList.add("match"); 
+                secondCard.parentNode.parentNode.classList.add("match"); 
+                firstCard=null; 
+                secondCard=null;
+                counter++; 
+                if (checkEnd()){ // check if the game is over
+                    clearInterval(myTimer); // ends the timer
+                    setTimeout(finish,100);     
                 }
-            }else{ // caso as duas cartas não tenham a mesma imagem
-                // console.log("nao");
-                lock=true; // nao permite que o jogador selecione outras cartas
+            }else{ // if the two cards do not have the same image
+                lock=true; // does not allow the player to select other cards
                 setTimeout( () => {
-                    primeiraCarta.parentNode.parentNode.classList.remove("flip"); // desvira as cartas
-                    segundaCarta.parentNode.parentNode.classList.remove("flip"); // desvira as cartas
-                    primeiraCarta=null;
-                    segundaCarta=null;
-                    lock=false; // permite que o jogador selecione outras cartas
-                },1000); // deixa as cartas viradas por 1 segundo
-                contador++;
+                    firstCard.parentNode.parentNode.classList.remove("flip"); 
+                    secondCard.parentNode.parentNode.classList.remove("flip"); 
+                    firstCard=null;
+                    secondCard=null;
+                    lock=false; // allows the player to select other cards
+                },1000); // the cards are flipped over for 1 second
+                counter++;
             }
         }
-        // console.log(verificaFim());
     }
 }
 
-function verificaFim () {
-    // verifica se o jogo já terminou 
-    // retorna true se todas as cartas do tabuleiro estiverem viradas
-    let fim = true;
-    let cartas = document.querySelectorAll(".carta");
-    for (carta of cartas){
-        if (!carta.classList.contains("flip")){
-            fim=false
+function checkEnd () {
+    // check if the game is over
+    // returns true if all cards on the board are flipped
+    let end = true;
+    let board = document.querySelectorAll(".card");
+    for (card of board){
+        if (!card.classList.contains("flip")){
+            end=false
         }
     }
-    return fim
+    return end
+}
+
+function finish () {
+    alert(`[PT-BR] Acabou em ${counter*2} jogadas e em ${document.querySelector("header p").innerHTML} segundos!\n\n[EN] Finished in ${counter*2} moves and ${document.querySelector("header p").innerHTML} seconds!`);
+    const againOptions = ["s","y","n",null];
+    let again=prompt("[PT-BR] Quer jogar novamente? (S ou N)\n\n[EN] Want to play again? (Y or N)").toLowerCase() ;
+    while (!againOptions.includes(again)){ 
+        again=prompt("[PT-BR] Quer jogar novamente? (S ou N)\n\n[EN] Want to play again? (Y or N)").toLowerCase();
+    }
+    if(again=="s" || again=="y"){ 
+        start();
+    }
 }
 
 
-iniciar(); // inicia o jogo
+start();
